dbee88aaecbe292bc39fefa5719ea783ba38d409
lucan <lucanlc0417@gmail.com>
2023-07-15 21:18:39 +0800

    左侧菜单静态搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,44 @@
         <!-- 左侧菜单 -->
         <div class="left">
             <Logo></Logo>
+            <!-- 菜单 -->
+            <el-scrollbar class="scrollbar">
+                <el-menu default-active="1" class="el-menu-vertical-demo" background-color="#001529" text-color="#fff">
+                    <el-menu-item index="1">
+                        <el-icon>
+                            <House />
+                        </el-icon>
+                        <span>首页</span>
+                    </el-menu-item>
+                    <el-menu-item index="2">
+                        <el-icon>
+                            <DataBoard />
+                        </el-icon>
+                        <span>数据大屏</span>
+                    </el-menu-item>
+                    <el-sub-menu>
+                        <template #title>
+                            <el-icon>
+                                <Lock />
+                            </el-icon>
+                            <span>权限管理</span>
+                        </template>
+                        <el-menu-item index="3-1">item one</el-menu-item>
+                        <el-menu-item index="3-2">item two</el-menu-item>
+                        <el-menu-item index="3-3">item two</el-menu-item>
+                    </el-sub-menu>
+                    <el-sub-menu>
+                        <template #title>
+                            <el-icon>
+                                <ShoppingBag />
+                            </el-icon>
+                            <span>商品管理</span>
+                        </template>
+                        <el-menu-item index="4-1">item one</el-menu-item>
+                        <el-menu-item index="4-2">item two</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </el-scrollbar>
         </div>
         <!-- 顶部导航 -->
         <div class="top">
@@ -17,6 +55,12 @@
 
 <script setup lang="ts">
 import Logo from './logo/index.vue'
+import {
+    House,
+    DataBoard,
+    Lock,
+    ShoppingBag,
+} from '@element-plus/icons-vue'
 </script>
 
 <style scoped lang="scss">
@@ -29,6 +73,11 @@ import Logo from './logo/index.vue'
         width: $leftMenuWidth;
         height: 100vh;
         padding: 20px;
+
+        .scrollbar {
+            height: calc(100vh - 80px);
+            border: nono;
+        }
     }
 
     .top {
