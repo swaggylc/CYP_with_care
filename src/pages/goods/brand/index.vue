324907db5064db31b3d81603da6f85746d1e18a4
lucan <lucanlc0417@gmail.com>
2023-07-18 21:17:20 +0800

    完成修改品牌业务

--- src/pages/goods/brand/index.vue
+++ src/pages/goods/brand/index.vue
@@ -17,19 +17,19 @@
                 </template>
             </el-table-column>
             <el-table-column label="品牌操作">
-                <template #default="scope">
-                    <el-button size="default" type="primary" icon="Edit" @click="editBrand">Edit</el-button>
+                <template #default="{ row }">
+                    <el-button size="default" type="primary" icon="Edit" @click="editBrand(row)">Edit</el-button>
                     <el-button size="default" type="danger" icon="Delete">Delete</el-button>
                 </template>
             </el-table-column>
         </el-table>
         <!-- 分页器 -->
         <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize" :page-sizes="[5, 10, 15, 20]"
-            layout=" prev, pager, next, jumper,->,total, sizes" :total="total" @current-change="changeCurrent"
+            layout=" prev, pager, next, jumper,->,total, sizes" :total="total" @current-change="GetBrandList"
             @size-change="changeSize" />
     </el-card>
     <!-- 对话框 -->
-    <el-dialog v-model="dialogFormVisible" title="添加品牌">
+    <el-dialog v-model="dialogFormVisible" :title="formData.id ? '修改品牌' : '添加品牌'">
         <el-form style="width: 70%;">
             <el-form-item label="品牌名称" label-width="80px">
                 <el-input placeholder="请输入品牌名称" v-model="formData.tmName"></el-input>
@@ -74,7 +74,8 @@ onMounted(() => {
 
 
 // 获取品牌列表的方法
-const GetBrandList = async () => {
+const GetBrandList = async (pager: number = 1) => {
+    currentPage.value = pager
     let res: IBrandListRes = await getBrandList(currentPage.value, pageSize.value)
     if (res.code === 200) {
         brandList.value = res.data.records
@@ -82,29 +83,27 @@ const GetBrandList = async () => {
     }
 }
 
-// 点击页码的回调
-const changeCurrent = (val: number) => {
-    currentPage.value = val
-    // 重新获取品牌列表
-    GetBrandList()
-}
 // 点击每页显示条数的回调
 const changeSize = (val: number) => {
     pageSize.value = val
-    currentPage.value = 1
     // 重新获取品牌列表
-    GetBrandList()
+    GetBrandList(1)
 }
 // 点击添加品牌按钮的回调
 const addBrand = () => {
     // 重置表单数据
     formData.logoUrl = ''
     formData.tmName = ''
+    if (formData.id) {
+        delete formData.id
+    }
     dialogFormVisible.value = true
 }
 // 点击修改品牌按钮的回调
-const editBrand = () => {
+const editBrand = (data: any) => {
     dialogFormVisible.value = true
+    // 将数据回显
+    Object.assign(formData, data)
 }
 // 点击取消按钮的回调
 const cancel = () => {
@@ -115,13 +114,13 @@ const confirm = async () => {
     dialogFormVisible.value = false
     let res: any = await addOrUpdateBrand(formData)
     if (res.code === 200) {
-        ElMessage.success('操作成功')
+        ElMessage.success(formData.id ? '修改成功' : '添加成功')
         // 关闭对话框
         dialogFormVisible.value = false
         // 重新获取品牌列表
-        GetBrandList()
+        GetBrandList(formData.id ? currentPage.value : 1)
     } else {
-        ElMessage.error('操作失败')
+        ElMessage.error(formData.id ? '修改失败' : '添加失败')
         // 关闭对话框
         dialogFormVisible.value = false
     }
