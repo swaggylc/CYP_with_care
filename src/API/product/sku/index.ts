c720781f7300f4fc616171ac4910d3b62e8d6a1e
lucan <lucanlc0417@gmail.com>
2023-07-25 20:19:56 +0800

    sku商品的上架与下架

--- src/API/product/sku/index.ts
+++ src/API/product/sku/index.ts
@@ -6,9 +6,21 @@ import type { SkuListRes } from './type'
 enum API {
   // 获取sku列表
   GETSKULIST_URL = '/admin/product/list/',
+  // 商品下架的接口
+  UNSALE_URL = '/admin/product/cancelSale/',
+  // 商品上架的接口
+  ONSALE_URL = '/admin/product/onSale/',
 }
 
 // 获取sku列表的方法
 export const getSkuList = (page: number, limit: number) => {
   return request.get<any, SkuListRes>(API.GETSKULIST_URL + `${page}/${limit}`)
+}
+// 上架商品的方法
+export const onSale = (skuId: number) => {
+  return request.get<any, any>(API.ONSALE_URL + skuId)
+}
+// 下架商品的方法
+export const unSale = (skuId: number) => {
+  return request.get<any, any>(API.UNSALE_URL + skuId)
 }
--- src/pages/goods/sku/index.vue
+++ src/pages/goods/sku/index.vue
@@ -22,8 +22,12 @@
       <el-table-column prop="price" label="价格(元)" width="250" />
       <el-table-column label="操作" fixed="right" width="300">
         <template #default="{ row }">
-          <el-button type="success" icon="Top"></el-button>
-          <el-button type="primary" icon="Edit"></el-button>
+          <el-button
+            type="success"
+            :icon="row.isSale == 1 ? 'Bottom' : 'Top'"
+            @click="changeSaleType(row)"
+          ></el-button>
+          <el-button type="primary" icon="Edit" @click="Edit"></el-button>
           <el-button type="info" icon="Platform"></el-button>
           <el-button type="danger" icon="Delete"></el-button>
         </template>
@@ -44,7 +48,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 // 引入接口
-import { getSkuList } from '@/API/product/sku/index.ts'
+import { getSkuList, onSale, unSale } from '@/API/product/sku/index.ts'
 // 引入ts类型
 import type { SkuListRes, SkuItem } from '@/API/product/sku/type.ts'
 import { ElMessage } from 'element-plus'
@@ -72,17 +76,39 @@ const getSkuListFn = async (pager: number = 1) => {
 }
 
 // 改变每页显示条数的回调
-const changeSize=(value:number)=>{
-    pageSize.value=value
-    getSkuListFn()
+const changeSize = (value: number) => {
+  pageSize.value = value
+  getSkuListFn()
 }
 
+// 点击上/下架按钮的回调
+const changeSaleType = async (row: SkuItem) => {
+  // 判断当前商品的上架状态，isSale=1表示已上架，需要调用下架方法；isSale=0表示下架，需要调用上架方法
+  if (row.isSale == 1) {
+    let res: any = await unSale(row.id)
+    if (res.code === 200) {
+      ElMessage.success('已下架!')
+      // 重新获取sku列表
+      getSkuListFn(currentPage.value)
+    } else {
+      ElMessage.error('下架失败!')
+    }
+  } else {
+    let res: any = await onSale(row.id)
+    if (res.code === 200) {
+      ElMessage.success('已上架!')
+      // 重新获取sku列表
+      getSkuListFn(currentPage.value)
+    } else {
+      ElMessage.error('上架失败!')
+    }
+  }
+}
 
-
-
-
-
-
+// 点击编辑按钮的回调
+const Edit = () => {
+  ElMessage.success('程序猿正在加班开发中...')
+}
 
 
 
