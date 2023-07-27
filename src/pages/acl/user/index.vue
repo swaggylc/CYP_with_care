73f145037a398504575c58c1b8159448188c2ebe
lucan <lucanlc0417@gmail.com>
2023-07-27 17:11:54 +0800

    添加/修改用户业务静态搭建

--- src/pages/acl/user/index.vue
+++ src/pages/acl/user/index.vue
@@ -11,10 +11,10 @@
     </el-form>
   </el-card>
   <el-card>
-    <el-button type="primary">添加</el-button>
+    <el-button type="primary" @click="addUser">添加</el-button>
     <el-button type="danger">批量删除</el-button>
     <el-table border style="margin: 20px 0" :data="userArr">
-      <el-table-column type="selection" width="80" />
+      <el-table-column type="selection" width="80" align="center" />
       <el-table-column
         label="#"
         width="100"
@@ -30,7 +30,9 @@
       <el-table-column label="操作" width="350" align="center">
         <template #="{ row, $index }">
           <el-button type="primary" icon="User">分配角色</el-button>
-          <el-button type="primary" icon="Edit">编辑</el-button>
+          <el-button type="primary" icon="Edit" @click="EditUser(row)">
+            编辑
+          </el-button>
           <el-button type="danger" icon="Delete">删除</el-button>
         </template>
       </el-table-column>
@@ -45,6 +47,23 @@
       @size-change="handleSizeChange"
     />
   </el-card>
+  <el-drawer v-model="visible" :show-close="true" size="25%" title="添加用户">
+    <el-form>
+      <el-form-item label="用户姓名">
+        <el-input placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="用户昵称">
+        <el-input placeholder="请输入用户昵称"></el-input>
+      </el-form-item>
+      <el-form-item label="用户密码">
+        <el-input placeholder="请输入用户密码"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer="">
+      <el-button type="primary">确定</el-button>
+      <el-button>取消</el-button>
+    </template>
+  </el-drawer>
 </template>
 
 <script setup lang="ts">
@@ -58,7 +77,9 @@ let currentPage = ref<number>(1)
 let pageSize = ref<number>(5)
 let total = ref<number>(0)
 
-let userArr = ref<User[]>([])
+let userArr = ref<User[]>([]) // 用户列表数据
+
+let visible = ref<boolean>(false) // 控制抽屉的显示隐藏
 
 onMounted(() => {
   GetUserList()
@@ -83,6 +104,28 @@ const handleSizeChange = (val: number) => {
   pageSize.value = val
   GetUserList()
 }
+
+// 点击添加用户按钮的回调
+const addUser = () => {
+  visible.value = true
+}
+
+// 点击编辑按钮的回调
+const EditUser = (row: User) => {
+  visible.value = true
+}
+
+
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <style scoped lang="scss">
